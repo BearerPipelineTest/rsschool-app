d721ee2df1c1548d08906652623e18e24e823e50
artsiom aliakseyenka <alekseenkoart@gmail.com>
2022-03-23 22:40:25 +0100

    connect login state with userid

--- server/src/models/loginState.ts
+++ server/src/models/loginState.ts
@@ -10,6 +10,10 @@ export class LoginState {
   @Index()
   createdDate: number;
 
+  @Index()
+  @Column({ nullable: true })
+  userId: number;
+
   @Column({ type: 'simple-json' })
   data: LoginData;
 }
